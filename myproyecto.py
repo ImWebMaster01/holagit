1310cf4ee20784848b86d5362331b45f45771516
wm01 <imwebmaster01@gmail.com>
2024-08-10 20:46:21 -0600

    inicio otravez

--- myproyecto.py
+++ myproyecto.py
@@ -1,1 +1,6 @@
-print("Hola Mundo desde pyton")
+print("Hola Mundo desde pyton 1")
+
+print("Hola Mundo desde pyton 2")
+print("Hola Mundo desde pyton 3")
+print("Hola Mundo desde pyton 4")
+print("Hola Mundo desde pyton 5")
